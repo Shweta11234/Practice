76c22c1f4a7439517e496e81b6590f34a8769d69
Shweta11234 <153056615+Shweta11234@users.noreply.github.com>
2023-12-06 14:23:09 +0530

    Update Option.java

--- src/Practice/Option.java
+++ src/Practice/Option.java
@@ -13,6 +13,8 @@ public class Option {
    while(true){
 	   System.out.println("---------Menu-----------");
 	   System.out.println("This is change done by other Developer");
+	   System.out.println("This is change done by me");
+	   
 	   System.out.println("1.Register Party");
 	   System.out.println("2.Display Party using partyseq");
 	   
